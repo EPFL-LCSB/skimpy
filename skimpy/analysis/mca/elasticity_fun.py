3ef66fb987cfbb46e2977a0e88e22d13425dd3d3
weilandt <daniel.weilandt@epfl.ch>
2018-02-28 08:31:59 +0100

    ADD: Created the backbone for the numerical computation of the elasticites

--- skimpy/analysis/mca/elasticity_fun.py
+++ skimpy/analysis/mca/elasticity_fun.py
@@ -24,3 +24,75 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 """
+
+from sympy import symbols, Array
+from sympy.utilities.autowrap import ufuncify
+
+
+
+class ElasticityFunction:
+    def _init_(self,
+               independent_variables,
+               dependent_variables,
+               expressions,
+               parameters):
+        """
+        Constructor for a precompiled function to compute elasticities
+        numerically
+        :param independent_variables: a list of strings denoting
+                                      the independent variables names
+        :param dependent_variables: a list of strings denoting
+                                      the dependent variables names
+        :param expressions: dict of sympy expressions for the rate of
+                            change of a variable indexed by the variable
+                            name
+        :param parameters: dict of parameters with parameter values
+
+        """
+        self.independent_variables = independent_variables
+        self.dependent_variables = dependent_variables
+        self.expressions = expressions
+        self.parameters = parameters
+
+
+        # Unpacking is needed as ufuncify only take ArrayTypes
+        the_param_keys = [x for x in self.parameters]
+        the_independent_variable_keys = [x for x in independent_variables]
+        the_dependent_variable_keys = [x for x in dependent_variables]
+
+        sym_vars = list(symbols(independent_variables+dependent_variables+the_param_keys))
+
+        # Sort the expressions
+        the_expressions = [self.expressions[x] for x in self.the_independent_variable_keys]
+
+        # Awsome sympy magic
+        # TODO problem with typs if any parameter ot vairabls is interpreted as interger
+        self.function = []
+        for exp in the_expressions:
+           self.function.append(ufuncify(tuple(sym_vars),
+                                         exp,
+                                         backend='Cython'))
+
+
+    @property
+    def parameters(self):
+        return self._parameters
+
+    @parameters.setter
+    def parameters(self,value):
+        """
+        Would-be optimization hack to avoid looking up thr whole dict at each
+        iteration step in __call__
+        :param value:
+        :return:
+
+        """
+        self._parameters = value
+        self.parameter_values = [x for x in self.parameters.values()]
+
+    def __call__(self,
+                 independent_variables,
+                 dependent_variables,
+                 expressions,
+                 parameters):
+        pass
