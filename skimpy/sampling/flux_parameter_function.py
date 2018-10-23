3eda0c3e185881ae5834d4053c3e5c0d5e7a04f2
weilandt <daniel.weilandt@epfl.ch>
2018-10-23 11:18:05 +0200

    MTN: Dont simplfy flux functions for time

--- skimpy/sampling/flux_parameter_function.py
+++ skimpy/sampling/flux_parameter_function.py
@@ -44,7 +44,7 @@ class FluxParameterFunction():
                              for rxn in model.reactions.values()]
 
         sym_vars = self.sym_parameters+self.sym_concentrations
-        self.function = make_cython_function(sym_vars, self.expressions, simplify=True)
+        self.function = make_cython_function(sym_vars, self.expressions, simplify=False)
 
     def __call__(self,
                  model,
