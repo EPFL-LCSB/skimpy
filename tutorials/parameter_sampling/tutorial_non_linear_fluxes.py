e910ed4f99149df987aa22dd6a98c3c6dae0c3e5
Daniel R. Weilandt <daniel.weilandt@epfl.ch>
2019-04-25 17:21:52 +0200

    FIX: Add numpy import

--- tutorials/parameter_sampling/tutorial_non_linear_fluxes.py
+++ tutorials/parameter_sampling/tutorial_non_linear_fluxes.py
@@ -24,7 +24,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 """
-
+import numpy as np
 # Test models
 from skimpy.core import *
 from skimpy.mechanisms import *
