7610ef54bbafc024ec1cc22baa9bf7cf8e52046a
Diane Bernard-Bruls <diane.bernard-bruls@epfl.ch>
2020-03-27 14:11:04 +0100

    FIX numpy iterator

--- skimpy/analysis/mca/utils.py
+++ skimpy/analysis/mca/utils.py
@@ -86,10 +86,7 @@ def get_reduced_stoichiometry(kinetic_model, all_variables, all_dependent_ix=Non
 
     if left_basis.any():
 
-        if left_basis.shape[0] == 1:
-            L0 = Matrix(left_basis)
-        else:
-            L0 = Matrix([x for x in left_basis])
+        L0 = Matrix(left_basis)
 
         ## We need to separate N and N0 beforehand
 
