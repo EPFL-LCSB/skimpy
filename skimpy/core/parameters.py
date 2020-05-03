8ff6bea4ef4f73bc5a405f218b0eacb920cce994
Daniel <daniel.weilandt@epfl.ch>
2020-05-03 21:12:54 +0200

    ADD: HDF5 output for parameter populations

--- skimpy/core/parameters.py
+++ skimpy/core/parameters.py
@@ -28,12 +28,15 @@ limitations under the License.
 from skimpy.utils.tabdict import TabDict
 from sympy import Symbol
 from pandas import Series
+import h5py
+
+import numpy as np
 
 class ParameterValues(object):
     """
-    Parameters set for kinetic models wichan be indexed with symbols or
+    Parameters set for kinetic models wich can be indexed with symbols or
     """
-    def __init__(self,kmodel,parameter_values):
+    def __init__(self,parameter_values, kmodel=None):
         """
 
         :param kmodel: KineticModel class
@@ -43,11 +46,18 @@ class ParameterValues(object):
             parameter_values = parameter_values.to_dict()
 
         self._parameter_values = TabDict([(str(p),v) for p,v in parameter_values.items()])
-        # Check if this is a good solution
-        model_params = kmodel.parameters
 
-        self._sym_to_str = {model_params[p].symbol:p for p in self._parameter_values}
-        self._str_to_param = {p:model_params[p] for p in self._parameter_values}
+        # Check if this is a good solution
+
+        if kmodel is not None:
+            model_params =  kmodel.parameters
+            self._str_to_sym = { p:model_params[p].symbol for p in model_params}
+        else:
+            self._str_to_sym = { p:Symbol(p) for p in parameter_values}
+
+        self._sym_to_str = {self._str_to_sym[p]:p for p in self._parameter_values}
+
+        #self._str_to_param = {p:model_params[p] for p in self._parameter_values}
 
     def __getitem__(self, item):
         if item.__class__ is  str:
@@ -63,3 +73,55 @@ class ParameterValues(object):
 
     def values(self):
         return  self._parameter_values.values()
+
+
+class ParameterValuePopulation(object):
+    def __init__(self,data, kmodel=None):
+        if type(data) == list:
+            # Todo check for indexable
+            self._data = [ ParameterValues(d,kmodel=kmodel) for d in data]
+
+        elif type(data) == pd.DataFrame():
+            pass
+        else:
+            raise TypeError("Type {} is not supported".format(type(data)))
+
+    def __getitem__(self,  index):
+        return self._data[index]
+
+    def save(self,filename):
+        f = h5py.File(filename, 'w') #TODO catch existing file?
+
+        # TODO more central way ?
+        param_names = np.array([k for k,v  in self._data[0]._parameter_values.items() if not (v is None)],
+                               dtype=object)
+        string_dt = h5py.special_dtype(vlen=str)
+
+        f.create_dataset('parameter_names', data=param_names, dtype=string_dt)
+        f.create_dataset('num_parameters_sets', data=len(self._data))
+
+        for i,this_data in enumerate(self._data):
+            this_data = np.array([this_data._parameter_values[p]
+                                 for p in param_names], dtype=np.float)
+            f.create_dataset('parameter_set_{}'.format(i), data=this_data)
+
+        f.close()
+
+
+## TODO Lets see this should maybe
+def load_parameter_population(filename):
+    f = h5py.File(filename, 'r')
+    data = []
+    param_names = np.array(f.get('parameter_names'))
+    parameters_sets = np.array(f.get('num_parameters_sets'))
+    for i in range(parameters_sets):
+        this_param_set = 'parameter_set_{}'.format(i)
+        param_values = np.array(f.get(this_param_set))
+        this_data = {k:v for k,v in zip(param_names,param_values)}
+        data.append(this_data)
+    f.close()
+
+    param_population = ParameterValuePopulation(data)
+
+    return param_population
+
