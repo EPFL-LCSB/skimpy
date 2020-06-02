f558f204d69d851ce4b708876dc816819208aebc
Daniel <daniel.weilandt@epfl.ch>
2020-06-02 11:04:55 +0200

    ADD: Gurobu relaxation function for minflux reuirement

--- skimpy/analysis/oracle/minimum_fluxes.py
+++ skimpy/analysis/oracle/minimum_fluxes.py
@@ -29,7 +29,7 @@ limitations under the License.
 from pytfa.analysis import variability_analysis
 
 
-def add_min_flux_requirements(tmodel,flux, inplace=True, safe=True):
+def add_min_flux_requirements(tmodel,flux, inplace=True, safe=True, exclude=[]):
     if inplace:
         temp_model = tmodel
     else:
@@ -40,37 +40,63 @@ def add_min_flux_requirements(tmodel,flux, inplace=True, safe=True):
         # Make variability analysis to check which reaction can carry the flux
         tva_fluxes = variability_analysis(temp_model, kind='reactions')
 
-        blocked_rxns = tva_fluxes[(tva_fluxes['minimum'] > -flux) &
-                                  (tva_fluxes['maximum'] < flux)].index
+        blocked_rxns = tva_fluxes[(tva_fluxes['minimum'] > -flux ) &
+                                  (tva_fluxes['maximum'] < flux  ) ].index
 
+        blocked_rxns = [b for b in blocked_rxns if b not in exclude]
         # Remove the reactions that cant carry the minimum flux requirement
         temp_model.remove_reactions([temp_model.reactions.get_by_id(rxn)
                                  for rxn in blocked_rxns])
 
         for rxn in temp_model.reactions:
-            rev_var = rxn.reverse_variable
-            fwd_var = rxn.forward_variable
+            if rxn.id not in exclude:
+                rev_var = rxn.reverse_variable
+                fwd_var = rxn.forward_variable
 
-            if flux <= rev_var.ub \
-                and rev_var.lb < flux \
-                and tva_fluxes['minimum'][rxn.id] < -flux:
-                rev_var.lb = flux
-            if flux <= fwd_var.ub \
-                and fwd_var.lb < flux \
-                and tva_fluxes['maximum'][rxn.id] > flux:
-                fwd_var.lb = flux
-
-
+                if flux <= rev_var.ub \
+                    and rev_var.lb < flux \
+                    and tva_fluxes['minimum'][rxn.id] < -flux:
+                    rev_var.lb = flux
+                if flux <= fwd_var.ub \
+                    and fwd_var.lb < flux \
+                    and tva_fluxes['maximum'][rxn.id] > flux:
+                    fwd_var.lb = flux
+    
     else:
         for rxn in temp_model.reactions:
-            rev_var = rxn.reverse_variable
-            fwd_var = rxn.forward_variable
+            if rxn.id not in exclude:
+                rev_var = rxn.reverse_variable
+                fwd_var = rxn.forward_variable
 
-            if flux <= rev_var.ub and rev_var.lb < flux:
-                rev_var.lb = flux
-            if flux <= fwd_var.ub and fwd_var.lb < flux:
-                fwd_var.lb = flux
+                if flux <= rev_var.ub and rev_var.lb < flux:
+                    rev_var.lb = flux
+                if flux <= fwd_var.ub and fwd_var.lb < flux:
+                    fwd_var.lb = flux
 
     temp_model.repair()
 
     return temp_model
+
+def relax_min_flux_gurobi(model, relax_obj_type = 0, exclude_variables=None):
+    N = len(model.reactions)
+    # Can we exclude the biomass reaction?
+    if exclude_variables is None:
+        the_vars = [x._internal_variable for x in model.variables[:2*N]]
+    else:
+        the_vars = [x._internal_variable for x in model.variables[:2 * N]
+                    if x not in exclude_variables ]
+
+    vars_penalities = [1]*len(the_vars)
+
+    grm = model.solver.problem.feasRelax(relaxobjtype=relax_obj_type,
+                                         minrelax=True,
+                                         constrs=None,
+                                         vars=the_vars,
+                                         lbpen=vars_penalities,
+                                         ubpen=None,
+                                         rhspen=None)
+
+    return grm
+
+# TODO Flux relaxation function for any solver see pytfa optim relax_LC!
+
