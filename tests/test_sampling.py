ae04f148f6f3613b8c1a94771fcbb0a2fe227816
Daniel <daniel.weilandt@epfl.ch>
2020-06-23 12:54:32 +0200

    FIX: Use concentrations not at equilibrium for the first reaction.

--- tests/test_sampling.py
+++ tests/test_sampling.py
@@ -1,8 +1,9 @@
 import pytest
 # Test models
-from skimpy.mechanisms import *
 from skimpy.sampling.simple_parameter_sampler import SimpleParameterSampler
+from skimpy.utils.namespace import *
 from tests.utils import build_linear_pathway_model
+
 
 
 @pytest.mark.dependency(name='build_linear_pathway_model')
@@ -14,7 +15,7 @@ def test_parameter_sampling_linear_pathway():
 
 
     flux_dict = {'E1': 1.0, 'E2': 1.0, 'E3': 1.0}
-    concentration_dict = {'A': 3.0, 'B': 2.0, 'C': 1.0, 'D': 0.5}
+    concentration_dict = {'A': 10.0, 'B': 5.0, 'C': 1.0, 'D': 0.05}
 
 
     parameters = SimpleParameterSampler.Parameters(n_samples=10)
