9b5a756d9cdae42827a90257e95e47a04dfd47b9
Daniel R. Weilandt <dw6835@princeton.edu>
2022-03-08 14:45:15 -0500

    FIX: Install Sundials 5.1.0 for latest ode-scikit version #7

--- docker/utils/install_sundials.sh
+++ docker/utils/install_sundials.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
-wget https://github.com/LLNL/sundials/releases/download/v3.1.1/sundials-3.1.1.tar.gz
-tar -xzf sundials-3.1.1.tar.gz -C $HOME
-cd $HOME/sundials-3.1.1
+# Sundails 5.1.0
+#wget https://github.com/LLNL/sundials/releases/download/v3.1.1/sundials-3.1.1.tar.gz
+#tar -xzf sundials-3.1.1.tar.gz -C $HOME
+#cd $HOME/sundials-3.1.1
+#
+#mkdir $HOME/sundials-3.1.1/builddir
+#cd $HOME/sundials-3.1.1/builddir
+#cmake -DLAPACK_ENABLE=ON \
+#      -DSUNDIALS_INDEX_SIZE=64 \
+#      -DCMAKE_INSTALL_PREFIX=$HOME/sundials-3.1.1 \
+#      $HOME/sundials-3.1.1
+#
+#make
+#make install
+#
+#
+#ls $HOME/sundials-3.1.1/include
 
-mkdir $HOME/sundials-3.1.1/builddir
-cd $HOME/sundials-3.1.1/builddir
+# Latest scikits odes distribution needs Sundails 5.1.0
+
+wget https://github.com/LLNL/sundials/releases/download/v5.1.0/sundials-5.1.0.tar.gz
+tar -xzf sundials-5.1.0.tar.gz -C $HOME
+cd $HOME/sundials-5.1.0
+
+mkdir $HOME/build-sundials-5.1.0
+cd $HOME/build-sundials-5.1.0/
 cmake -DLAPACK_ENABLE=ON \
       -DSUNDIALS_INDEX_SIZE=64 \
-      -DCMAKE_INSTALL_PREFIX=$HOME/sundials-3.1.1 \
-      $HOME/sundials-3.1.1
+      -DCMAKE_INSTALL_PREFIX=$HOME/sundials-5.1.0/ \
+      $HOME/sundials-5.1.0/
 
-make
 make install
 
+ls  $HOME/sundials-5.1.0/include
 
-ls $HOME/sundials-3.1.1/include
