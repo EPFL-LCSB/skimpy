bfc8c9785e24011eecac6fab23a9feeca519e294
Daniel Weilandt <daniel.weilandt@princeton.edu>
2022-05-02 13:58:54 -0400

    FIX: Tutorial boundaries, remove deprecated feature ELEMENTARY

--- tutorials/basics/tutorial_boundaries.py
+++ tutorials/basics/tutorial_boundaries.py
@@ -35,11 +35,10 @@ metabolites = ReversibleMichaelisMenten.Reactants(substrate='A', product='B')
 
 ## ELEMENTARY Method
 parameters = ReversibleMichaelisMenten.Parameters(
-    vmax_forward = 1,
-    vmax_backward = 0.5,
-    km_substrate = 10,
-    km_product = 10,
-    total_enzyme_concentration = 1,
+    vmax_forward=1,
+    k_equilibrium=1,
+    km_substrate=10,
+    km_product=10,
 )
 
 import warnings
@@ -55,22 +54,16 @@ this_model.add_reaction(pfk)
 this_model.parametrize_by_reaction({pfk.name:parameters})
 
 ## Make the Boundary Condition
-
 the_boundary_condition = ConstantConcentration(this_model.reactants['A'])
-# -- OR -- reactants ?
-# TODO
 
 this_model.add_boundary_condition(the_boundary_condition)
 
-## Full rate method
+this_model.parameters.A.value = 10.0
 
+this_model.compile_ode()
 
-this_model.compile_ode(sim_type = ELEMENTARY)
-
-this_model.initial_conditions.A = 10.0
 this_model.initial_conditions.B = 1.0
-this_model.initial_conditions.pfk = 1.0
 
 this_sol_full = this_model.solve_ode([0, 100.0], solver_type='cvode')
 
-this_sol_full.plot('output/boundary_out_full.html')
+this_sol_full.plot('boundary_out_full.html')
